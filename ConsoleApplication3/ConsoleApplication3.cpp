6a777ffd18285b5dc0b11d4041d8f02acecca35f
MhdDnl <danielrosli019@gmail.com>
2022-07-10 19:56:03 +0800

    Login.h and login.cpp added.

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,39 +1,11 @@
 #include<iostream>
-#include <stdlib.h>
-#include <Windows.h>
-#include <string>
 #include "app.h"
+#include "Login.h"
 
 using namespace std;
-#include <string.h>
-
-
-
-bool login() {
-	string username;
-	int password;
-	int count = 0;
-	while (count < 3) {
-		cout << "Please enter your username:"; cin >> username;
-		cout << "Please enter your password:"; cin >> password;
-		if (password == 123 && username == "up") {
-			return true;
-		}
-		else {
-			count++;
-			cout << "Wrong username or password\n";
-		}
-
-	}
-	return false;
-
-}
-
 
 int main()
 {
-
-
 	bool registered = login();
 
 	if (registered) {
@@ -45,12 +17,11 @@ int main()
 		cout << "          Thanks For Using Our System          " << endl;
 		cout << "===============================================" << endl;
 		cout << "You been kicked out from system due to false " << endl;
-		cout << "id number and password,Please try again" << endl;
+		cout << "username and password,Please try again" << endl;
 		cout << "                                               " << endl;
 		cout << "                                               " << endl;
 		cout << "Note:You have 3 login attempt                  " << endl;
 		cout << "===============================================" << endl;
-		// exit
 	}
 	
 }
